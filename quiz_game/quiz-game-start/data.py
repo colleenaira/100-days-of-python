dde1b37eb04d334b1bd4fa1f54ea7fddd95c3bfa
kolin <40494596+colleenaira@users.noreply.github.com>
2023-07-29 18:48:56 -0700

    Add files via upload

---
+++ quiz_game/quiz-game-start/data.py
@@ -0,0 +1,38 @@
+question_data = [
+         {
+             "category": "Entertainment: Video Games",
+             "type": "boolean", "difficulty": "easy",
+             "question": "There are 6 legendary cards in Clash Royale.",
+             "correct_answer": "False",
+             "incorrect_answers": ["True"]
+         },
+         {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+          "question": "Pac-Man was invented by the designer Toru Iwatani while he was eating pizza.",
+          "correct_answer": "True", "incorrect_answers": ["False"]},
+         {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+          "question": "Tetris is the #1 best-selling video game of all-time.", "correct_answer": "False",
+          "incorrect_answers": ["True"]},
+         {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+          "question": "Gordon Freeman, the protagonist of Half-Life is said to have once had a ponytail.",
+          "correct_answer": "True", "incorrect_answers": ["False"]},
+         {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+          "question": "In Pok&eacute;mon Sun and Moon, a male Salandit can evolve to a Salazzle.",
+          "correct_answer": "False",
+          "incorrect_answers": ["True"]},
+         {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+          "question": "In Pok&eacute;mon, Arbok evolves into Seviper.",
+          "correct_answer": "False", "incorrect_answers": ["True"]},
+         {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+          "question": "The ghosts in Pac-Man&quot and Ms. Pac-Man have completely different behavior.",
+          "correct_answer": "True", "incorrect_answers": ["False"]},
+         {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+          "question": "Activision created Battlefield 1.", "correct_answer": "False", "incorrect_answers": ["True"]},
+         {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+          "question": "Doki Doki Literature Club was developed in Japan.", "correct_answer": "False",
+          "incorrect_answers": ["True"]},
+         {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+          "question": "In the video game Splatoon;, the playable characters were originally going to be rabbits instead of squids.",
+          "correct_answer": "True", "incorrect_answers": ["False"]}
+
+
+]
